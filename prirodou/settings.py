550b978aaa9cc1b15d8c37c6d28c6893ff99bcda
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-04-10 12:44:06 +0200

    Initialize Django project with basic configuration
    Set up a new Django project named "prirodou" with initial configuration files, including ASGI, WSGI, settings, and URL routing. Added IntelliJ IDEA project files for Python environment setup and PostGIS database integration.

---
+++ prirodou/settings.py
@@ -0,0 +1,86 @@
+from decouple import config
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", cast=bool)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda x: x.split(","))
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.gis",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "prirodou.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "prirodou.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = config("LANGUAGE_CODE")
+TIME_ZONE = config("TIME_ZONE")
+USE_I18N = config("USE_I18N", cast=bool)
+USE_TZ = config("USE_TZ", cast=bool)
+
+STATIC_URL = config("STATIC_URL")
+STATIC_ROOT = config("STATIC_ROOT")
+
+MEDIA_URL = config("MEDIA_URL")
+MEDIA_ROOT = config("MEDIA_ROOT")
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
