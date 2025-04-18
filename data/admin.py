82511c775f61e4a750a5e82a8d3b234f384b297a
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-04-18 09:39:12 +0200

    Add Article and Author models with SFTP storage setup
    Introduced `Article` and `Author` models along with admin configurations. Added SFTP storage configuration for handling media files. Updated dependencies and settings to support the new features.

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models.article import Article
+from .models.author import Author
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Admin panel configuration for Author model.
+    """
+    list_display = ("author_name",)
+    search_fields = ("author_name",)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    Admin panel configuration for Article model.
+    """
+    list_display = ("taxon", "citation", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("citation", "authors__author_name", "taxon__taxon_name")
+    autocomplete_fields = ("taxon", "authors")
+    filter_horizontal = ("authors",)
+    fieldsets = (
+        (None, {
+            "fields": ("taxon", "authors")
+        }),
+        ("Article Details", {
+            "fields": ("citation", "document")
+        }),
+        ("Timestamps", {
+            "fields": ("created_at", "updated_at"),
+            "classes": ("collapse",),
+        }),
+    )
+    readonly_fields = ("created_at", "updated_at")
